9712638fb4e1b56520635a78dd4b42693619ecff
faz <faz@fazels-MacBook-Pro.local>
2018-11-25 17:44:27 +0330

    bug fix

--- app/src/main/java/com/faz/imagesviewer/utils/AppConstants.kt
+++ app/src/main/java/com/faz/imagesviewer/utils/AppConstants.kt
@@ -4,9 +4,10 @@ object AppConstants {
 
     internal val APP_DB_NAME = "images.db"
     internal val PREF_NAME = "imagesviewer_pref"
+    internal val NULL_INDEX = -1L
 
     enum class LoggedInMode constructor(val type: Int) {
         LOGGED_IN_MODE_LOGGED_OUT(0),
-        LOGGED_IN_MODE_SERVER(1)
+        LOGGED_IN_MODE_SERVER(3)
     }
 }
